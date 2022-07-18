86d841f0da247f765cc3bde67e986ef5d6bc9d35
Gonzalo Mancuelo Leites <gonlm33@gmail.com>
2022-07-18 00:57:00 -0300

    menu e-commerce

---
+++ src/component/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const navBar = () => {
+    return (
+        <div>
+
+            <button className="NavBar__button">Menu</button>
+            <button className="NavBar__button">Productos</button>
+            <button className="NavBar__button">Sobre Nosotro</button>
+            <button className="NavBar__button">Preguntas frecuentes</button>
+
+        </div>
+    )
+}
+
+export default navBar
