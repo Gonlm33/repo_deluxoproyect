38d96b64bf8f95e452ef98847c8912d62f271666
Gonzalo Mancuelo Leites <gonlm33@gmail.com>
2022-07-25 18:33:41 -0300

    contador con boton

--- src/component/ItemListContainer/ItemListContainer.jsx
+++ src/component/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,25 @@
+import ItemCount from "../ItemCount/itemCount"
 import React from 'react'
 import Articulos from "../Articulos/Articulos";
+import swal from "sweetalert";
 function ItemListContainer() {
+
+  const onAdd = (quantity) => {
+  swal({
+    title: "Compra",
+    text: "Compraste ${quantity} unidades",
+    icon: "success",
+    button: "Aceptar",
+  });
+  }
+
+
   return (
+    <>
     <Articulos greeting="Deluxo Disqueria Multiversal" />
-  )
+    <ItemCount initial={1} stock={10} onAdd={onAdd}  />
+    </>
+  );
 }
 
 export default ItemListContainer;
